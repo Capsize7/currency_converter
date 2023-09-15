4bd6e3f3dedf455c2454ac9c96f02ab089750990
Roman <135245474+Capsize7@users.noreply.github.com>
2023-09-15 09:19:44 +0400

    Update serializers.py

--- converter/converter_api/serializers.py
+++ converter/converter_api/serializers.py
@@ -4,12 +4,16 @@ from converter_api.models import Converter, ConverterSource
 
 
 class ConverterSerializer(ModelSerializer):
+    '''Simple model based serializer for pairs of currencies conversion'''
+    
     class Meta:
         model = Converter
         fields = ['to_currency', 'from_currency', 'created', 'amount']
 
 
 class ConverterSourceSerializer(ModelSerializer):
+    '''Simple model based serializer for source data of currencies conversion'''
+    
     class Meta:
         model = ConverterSource
         fields = ['currencies', 'created']
